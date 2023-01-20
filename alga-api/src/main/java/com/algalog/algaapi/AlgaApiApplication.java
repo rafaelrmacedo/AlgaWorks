60777889308cfba86e22af22a2dca3739dc539a2
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2023-01-20 08:25:55 -0300

    first project files

---
+++ alga-api/src/main/java/com/algalog/algaapi/AlgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algalog.algaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaApiApplication.class, args);
+	}
+
+}
---
+++ alga-api/src/test/java/com/algalog/algaapi/AlgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algalog.algaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
